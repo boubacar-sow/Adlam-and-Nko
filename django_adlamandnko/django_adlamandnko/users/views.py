fa9c51294239e105f3921031ef42f6393ede70e0
boubacar-sow <72445986+boubacar-sow@users.noreply.github.com>
2024-10-14 03:09:56 +0200

    Improve the frontend of the register page

--- django_adlamandnko/django_adlamandnko/users/views.py
+++ django_adlamandnko/django_adlamandnko/users/views.py
@@ -2,30 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout, login
-
-
+from django.contrib.auth import login, logout
+from django.http import JsonResponse
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()  # Save the new user
+            user = form.save()
             login(request, user)
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-
-            return redirect('profile')
+            messages.success(request, f'Account created for {user.username}!')
+            return JsonResponse({'success': True, 'redirect_url': '/profile/'})
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'success': False, 'error': errors})
     else:
         form = UserRegisterForm()
-    
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
 
-@login_required  # Only allow access to logged-in users
+@login_required
 def custom_logout(request):
     logout(request)
     return redirect('home')
